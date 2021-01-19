27b04fd06f6a8fa11acc8cc7f0a8b62ea78cea1c
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2021-01-19 17:33:05 +0500

    some changes

--- src/screens/Contract21/Form.jsx
+++ src/screens/Contract21/Form.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
+import Transport from "../../components/Transport";
 import Anketa from "./Anketa";
+
 const Form = () => {
     const [active, setActive] = useState(1)
     return (
-        <div className="stepwizard">
-            <ul>
-            {[1, 2, 3, 4].map((item, idx) => (
-                <button className={active == item ? 'active' : ''} key={idx}>item</button>
-            ))}
-            </ul>
-            {active == 1 ? <Anketa /> : ''}
+        <div className="form-main">
+            <div className="stepwizard">
+                <ul className="stepwizard-navigators">
+                    {[
+                        { id: 1, label: 'Общие сведения' },
+                        { id: 2, label: 'Объект' },
+                        { id: 3, label: 'Договор страхования' },
+                        { id: 4, label: 'Оплата' },
+                        { id: 5, label: 'Полис' }
+                    ].map((item, idx) => (
+                        <li className={active == item.id ? 'active' : ''} onClick={() => setActive(item.id)} key={idx}>
+                            <span>{item.id}</span>
+                            <strong>
+                                {item.label}
+                            </strong>
+                        </li>
+                    ))}
+                </ul>
+                {active == 1 && <Anketa />}
+                {active == 2 && <Transport />}
+            </div>
         </div>
     )
 }
