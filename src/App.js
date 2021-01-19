52ff10c7e45979707cf6d101156cea9adb1993c5
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2021-01-19 15:20:57 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Main} from './screens';
+import { Main } from './screens';
 import './assets/style.css';
-
+import './assets/dogovor.css';
+import { Form } from "./screens/Contract21";
 function App() {
   return (
-    <Main/>
+    // <Main/>
+    <Form />
   );
 }
 
